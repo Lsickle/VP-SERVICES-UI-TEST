45850396837dccd85a47b6ad5d6668d31b6315e3
Sebaxx77 <juanpiamba23@gmail.com>
2025-03-25 14:42:50 -0500

    Se agrega el trait HasRoles de Spatie y se hace uso en el modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles; // Se agrega el trait HasRoles de Spatie
 
 class User extends Authenticatable
 {
@@ -17,6 +18,7 @@ class User extends Authenticatable
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles; // Se hace uso de HasRoles
 
     /**
      * The attributes that are mass assignable.
