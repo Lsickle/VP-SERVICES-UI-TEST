820c9a3c3fc921d4966bfac9e5eba3117ed28846
Sebaxx77 <juanpiamba23@gmail.com>
2025-03-27 10:02:26 -0500

    Se modifica el archivo global.js con el scritp para el sidebar y se agrega un evento para manejar el cierre del side bar cuando se pulse fuera de este en movil.

--- resources/js/global.js
+++ resources/js/global.js
@@ -1,13 +1,26 @@
-// Archvio global para scripts
-document.addEventListener('DOMContentLoaded', function () {
-    const sidebarToggle = document.getElementById('sidebarToggle');
-    const sidebar = document.getElementById('sidebar');
-    const mainContent = document.getElementById('mainContent');
+// Archvio global para scripts 
 
-    if(sidebarToggle && sidebar && mainContent) {
-        sidebarToggle.addEventListener('click', function() {
-            sidebar.classList.toggle('hidden'); // Muestra u oculta el sidebar
-            mainContent.classList.toggle('ml-64'); // Desplaza el contenido cuando el sidebar está visible
+//Se maneja el Sidebar
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebarToggle = document.getElementById("sidebarToggle");
+    const sidebar = document.getElementById("sidebar");
+
+    if (sidebarToggle && sidebar) {
+        // Listener para mostrar/ocultar el sidebar al hacer clic en el botón
+        sidebarToggle.addEventListener("click", function (event) {
+            event.stopPropagation(); // Evita que el clic se propague al documento
+            sidebar.classList.toggle("-translate-x-full");
+        });
+
+        // Listener para cerrar el sidebar si se hace clic fuera de él
+        document.addEventListener("click", function (event) {
+            // Verifica que el sidebar esté visible (sin la clase -translate-x-full)
+            if (!sidebar.classList.contains("-translate-x-full")) {
+                // Si el clic se realiza fuera del sidebar y del botón toggle
+                if (!sidebar.contains(event.target) && !sidebarToggle.contains(event.target)) {
+                    sidebar.classList.add("-translate-x-full");
+                }
+            }
         });
     }
 });
