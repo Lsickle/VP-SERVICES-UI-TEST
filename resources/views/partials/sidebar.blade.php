4a2ede39bbb86513c2214faec7f938401645b064
Sebaxx77 <juanpiamba23@gmail.com>
2025-03-27 09:50:44 -0500

    Se ajusta las clase tailwind del side bar para que no interfiera con otros componentes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- Sidebar -->
 <div id="sidebar"
-  class="fixed inset-y-0 left-0 bg-gray-800 text-white w-64 p-5 transform -translate-x-full md:translate-x-0 md:relative transition-transform duration-300 ease-in-out">
+class="fixed top-0 left-0 bg-gray-800 text-white w-64 h-screen p-5 transform -translate-x-full md:translate-x-0 transition-transform duration-300 ease-in-out z-50">
   <ul>
     <li>
       <a href="{{ route('dashboard') }}" class="block p-2 hover:bg-gray-700">Inicio</a>
