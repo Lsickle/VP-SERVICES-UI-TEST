660357194a037002a83f1fc712d4fac1da497e61
Sebaxx77 <juanpiamba23@gmail.com>
2025-03-27 10:27:24 -0500

    Se modifica el sidebar para manejar mejor la dinamicidad segun el usuario autenticado tanto el no autenticado, se añade una clase a el sidebar para manejar el tamaño del texto en movil y el tamaño del contenedor.

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,12 +1,30 @@
 <!-- Sidebar -->
 <div id="sidebar"
-class="fixed top-0 left-0 bg-gray-800 text-white w-64 h-screen p-5 transform -translate-x-full md:translate-x-0 transition-transform duration-300 ease-in-out z-50">
-  <ul>
+  class="fixed top-0 left-0 bg-gray-800 text-white w-auto max-w-xs h-screen p-4 transform -translate-x-full md:translate-x-0 transition-transform duration-300 ease-in-out z-50">
+  <ul class="text-sm md:text-base">
+    @guest
+    <!-- Opciones para usuarios no autenticados -->
+    <h2 class="mb-2">Menu de Opciones</h2>
     <li>
-      <a href="{{ route('dashboard') }}" class="block p-2 hover:bg-gray-700">Inicio</a>
+      <a href="{{ route('bienvenido') }}" class="block p-2 hover:bg-gray-700">Home</a>
     </li>
+    <li>
+      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Iniciar Sesión</a>
+    </li>
+    <h2 class="mb-2">Agendamientos</h2>
+    <li>
+      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Agendar Visita</a>
+    </li>
+    <li>
+      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Descargo de Pedidos</a>
+    </li>
+    @endguest
+
+    @auth
+    <!-- Opciones para usuarios autenticados -->
     @if(request()->routeIs('dashboard.administrador'))
     @if(auth()->user()->hasRole('Administrador'))
+    <h2 class="mb-2">Panel Administrador</h2>
     <li>
       <a href="{{ route('usuarios.index') }}" class="block p-2 hover:bg-gray-700">Gestión de Usuarios</a>
     </li>
@@ -20,16 +38,12 @@ class="fixed top-0 left-0 bg-gray-800 text-white w-64 h-screen p-5 transform -tr
       <a href="{{ route('seguridad.permisos.index') }}" class="block p-2 hover:bg-gray-700">Permisos</a>
     </li>
     @endif
-    @elseif(request()->routeIs('bienvenido'))
+    @else
+    <!-- Otras opciones para usuarios autenticados en otras rutas -->
     <li>
-      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Opción 1</a>
-    </li>
-    <li>
-      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Opción 2</a>
-    </li>
-    <li>
-      <a href="{{ route('login') }}" class="block p-2 hover:bg-gray-700">Opción 3</a>
+      <a href="{{ route('dashboard') }}" class="block p-2 hover:bg-gray-700">Dashboard</a>
     </li>
     @endif
+    @endauth
   </ul>
 </div>
