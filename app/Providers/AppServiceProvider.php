b0878d22a03123cb927dfbc0dc4f5278dadd4294
Sebaxx77 <juanpiamba23@gmail.com>
2025-03-28 10:30:42 -0500

    Se agrega una function boot para mostrar la traduccion a español de el campo tomorrow cuando se use la regla de validación after or equal.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,8 +18,14 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
-    }
+    public function boot()
+{
+    Validator::replacer('after_or_equal', function ($message, $attribute, $rule, $parameters) {
+        // Si el parámetro es "tomorrow", lo cambiamos por "mañana"
+        if (isset($parameters[0]) && $parameters[0] === 'tomorrow') {
+            $parameters[0] = 'mañana';
+        }
+        return str_replace(':date', $parameters[0], $message);
+    });
+}
 }
